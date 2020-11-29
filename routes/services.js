20d314e8775d2accfc29bb3fdebe2f645a4bd65a
Allan Ramirez <ramirezag@gmail.com>
2020-11-29 15:38:53 +0100

    Removed unnecessary codes

--- routes/services.js
+++ routes/services.js
@@ -53,12 +53,6 @@ router.get('/', (req, res) => {
         .catch(errorHandler.handleError(res))
 })
 
-router.get('/:id/queues', (req, res) => {
-    return Services.getServiceDetails(req.params.id)
-      .then((service) => res.json(service))
-      .catch(errorHandler.handleError(res));
-});
-
 router.get('/:id', (req, res) => {
     return Services.getServiceDetails(req.params.id)
         .then((service) => res.json(service))
