66442cd52eab2bd13501830e9e871e89ea3616a1
SimonYu817 <71938726+SimonYu817@users.noreply.github.com>
2020-11-29 19:07:14 +0800

    Updated pagination

--- routes/services.js
+++ routes/services.js
@@ -29,8 +29,8 @@ router.post('/', (req, res) => {
 
 router.get('/', (req, res) => {
     let {pageNo, resultsPerPage} = req.query;
-    let pgNum = (pageNo) ? pageNo : 1;
-    let pgRes = (resultsPerPage) ? resultsPerPage : 10;
+    let pgNum = pageNo ? parseInt(pageNo) : 1;
+    let pgRes = resultsPerPage ? parseInt(resultsPerPage) : 10;
     return Services.getServices(pgNum, pgRes)
         .then((results) => res.json(results))
         .catch(errorHandler.handleError(res))
