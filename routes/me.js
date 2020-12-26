4fb0c6cfb1b0b2ea20cc640304e1955da8cec622
Allan Ramirez <ramirezag@gmail.com>
2020-12-26 17:16:58 +0100

    Added current user queue history

--- routes/me.js
+++ routes/me.js
@@ -3,9 +3,15 @@ const queueService = require('../services/queue.service');
 const errorHandler = require('./errorHandler');
 
 router.get('/queues/current', (req, res) => {
-  let {pageNo, resultsPerPage, status} = req.query;
-  status = status ? [status] : ['IN_QUEUE', 'IN_PROGRESS'];
-  return queueService.getUserQueues(req.user.id, parseInt(pageNo), parseInt(resultsPerPage), status)
+  let {pageNo, resultsPerPage} = req.query;
+  return queueService.getUserQueues(req.user.id, parseInt(pageNo), parseInt(resultsPerPage), ['IN_QUEUE', 'IN_PROGRESS'])
+    .then((results) => res.json(results))
+    .catch(errorHandler.handleError(res));
+});
+
+router.get('/queues/history', (req, res) => {
+  let {pageNo, resultsPerPage} = req.query;
+  return queueService.getUserQueues(req.user.id, parseInt(pageNo), parseInt(resultsPerPage), ['NO_SHOW', 'COMPLETED'])
     .then((results) => res.json(results))
     .catch(errorHandler.handleError(res));
 });
